824711fe2da914737019df47a51dc8c1a81709f4
johnjbarton <johnjbarton@johnjbarton.com>
2012-04-27 21:30:07 -0700

    try with submodules

--- prototype.sh
+++ prototype.sh
@@ -12,10 +12,11 @@ REMOTE_BRANCH="protocolExtension"
 REMOTE_PATH="front_end/"
 LOCAL_PATH="inspector/front_end"
 
-LOCAL_BRANCH=`git symbolic-ref HEAD`
+LOCAL_BRANCH=`git symbolic-ref HEAD | sed -e 's/^.*\///'`
 
-git checkout --orphan $REMOTE_PATH
+git checkout --orphan $PROJECT_NAME
 git rm -rf .
-git clone -b $REMOTE_BRANCH $REMOTE_URL
+git submodule add -b $REMOTE_BRANCH $REMOTE_URL
 
+#git checkout $LOCAL_BRANCH
 
