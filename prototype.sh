c190e1207560d457d698f5e7a2914ffb240b23c2
johnjbarton <johnjbarton@johnjbarton.com>
2012-04-30 21:10:50 -0700

    git-tx-clone works in prototype, remote with archive to transfer

--- prototype.sh
+++ prototype.sh
@@ -6,17 +6,32 @@ git clone git@github.com:johnjbarton/atopwi.git
 cd atopwi
 
 # git tx-clone [--name <projectName> ] <url> <branchname>  <remote_path> <local_path>
-PROJECT_NAME="front-end"
-REMOTE_URL="git@github.com:johnjbarton/front-end.git"
-REMOTE_BRANCH="protocolExtension"
-REMOTE_PATH="front_end/"
-LOCAL_PATH="inspector/front_end"
 
+# Must be unique
+#
+PROJECT_NAME="front-end"
+
+REMOTE_URL="git@github.com:johnjbarton/front-end.git"
+
+# Must not be blank, default to 'master'
+REMOTE_BRANCH="protocolExtension"
+
+# relative to the current working directory...in the *local* tree!
+# so we must be at root of the local tree
+#
+REMOTE_PATH=""
+
+# Path prefix must end in "/" 
+LOCAL_PATH="inspector/front-end/"
+
+# Strip refs/head/<branch> to give <branch>
+#
 LOCAL_BRANCH=`git symbolic-ref HEAD | sed -e 's/^.*\///'`
 
-git checkout --orphan $PROJECT_NAME
-git rm -rf .
-git submodule add -b $REMOTE_BRANCH $REMOTE_URL
+# Fetch only one branch into our specially named remote
+#
+git remote add -f -t $REMOTE_BRANCH tx.$PROJECT_NAME $REMOTE_URL
 
-#git checkout $LOCAL_BRANCH
-
+# move the remote subtree into our local tree
+#
+git archive --format=tar --prefix=$LOCAL_PATH refs/remotes/tx.$PROJECT_NAME/$REMOTE_BRANCH $REMOTE_PATH | tar xf -
